dd8635810e58276c2ac4c13652bbdabdbcf2cfbb
sophiebsw <sophiebsw@gmail.com>
2026-07-12 19:57:45 -0700

    load wp-config secrets from environment

--- wordpress/config/wp-config.php
+++ wordpress/config/wp-config.php
@@ -1,9 +1,9 @@
 <?php
 /** TODO should be handled properly, possibly by k8s secrets? */
-define( 'DB_NAME', 'meow' );
-define( 'DB_USER', 'meowuser' );
-define( 'DB_PASSWORD', 'meowpassword' );
-define( 'DB_HOST', 'mariadb' );
+define( 'DB_NAME', getenv('DB_NAME') );
+define( 'DB_USER', getenv('DB_USER') );
+define( 'DB_PASSWORD', getenv('DB_PASSWORD') );
+define( 'DB_HOST', getenv('DB_HOST') );
 define( 'DB_CHARSET', 'utf8mb4' );
 define( 'DB_COLLATE', '' );
 
